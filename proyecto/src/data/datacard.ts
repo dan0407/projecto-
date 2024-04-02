4aaf0175318f3fd4e410fdb78d5f2c6262bf98ef
daniel <145810043+dan0407@users.noreply.github.com>
2024-04-01 21:28:57 -0500

    Add files via upload

--- proyecto/src/data/datacard.ts
+++ proyecto/src/data/datacard.ts
@@ -18,4 +18,10 @@ export const datacard: DataShape[] = [
 			'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHA6uY_3CBJGQSypkC-G4lFVylqxZ79H9nocIdrYp0CF6WQaPdr_2-6fyZDrbUIqHZrNk&usqp=CAU',
 		image: 'https://www.superprof.es/blog/wp-content/uploads/2020/11/precio-centros-deportivos-scaled.jpg',
 	},
+	{
+		name: 'annabel_lu09',
+		profileImage: 'https://www.famousbirthdays.com/faces/lucinda-annabel-image.jpg',
+		image:
+			'https://cdn-az.allevents.in/events5/banners/b43a02e3de560e02f680057a428b26a13d83b4f2f73000c26b125604d34ba8bd-rimg-w1200-h600-gmir.jpg?v=1706588605',
+	},
 ];
