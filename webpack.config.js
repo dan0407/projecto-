38ad810545049b380cfcd2c64d8a62294dba79fb
dan0407 <duque.murillas@gmial.com>
2024-05-10 07:20:00 -0500

    pro3

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const path = require('path');
 
 module.exports = {
@@ -36,3 +37,43 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist'),
 	},
 };
+=======
+const path = require('path');
+
+module.exports = {
+	entry: './src/indexabuelo.ts',
+	mode: 'development',
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['css-loader'],
+			},
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.(png|jpe?g|gif)$/i,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							outputPath: 'images',
+						},
+					},
+				],
+			},
+			,
+		],
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+};
+>>>>>>> 00c6d9f (pro3)
