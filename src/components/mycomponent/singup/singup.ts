93ce2fd247712af77620987bca73cf53d8005736
Estudiante <estudiante@SC310E12.local>
2024-05-16 18:03:00 -0500

    pro36

--- src/components/mycomponent/singup/singup.ts
+++ src/components/mycomponent/singup/singup.ts
@@ -2,6 +2,7 @@ import styles from '../singup/singup.css';
 import { dispatch } from "../../../store/index";
 import { navigate } from "../../../store/actions";
 import { Screens } from "../../../types/trips";
+import { registrarUsuario } from '../../../utils/firebase';
 
 class singup extends HTMLElement {
 	constructor() {
@@ -35,7 +36,7 @@ handlelogButton() {
 			// Create the email label and input
 			const emailLabel = document.createElement('label');
 			emailLabel.setAttribute('for', 'email');
-			emailLabel.textContent = 'Email address or user name';
+			emailLabel.textContent = 'Email address ';
 
 			const emailInput = document.createElement('input');
 			emailInput.type = 'email';
@@ -61,7 +62,7 @@ handlelogButton() {
 			ageLabel.textContent = 'age';
 
 			const ageInput = document.createElement('input');
-			ageInput.type = 'text';
+			ageInput.type = 'number';
 			ageInput.id = 'age';
 
 			const BenchpressLabel = document.createElement('label');
@@ -90,31 +91,29 @@ handlelogButton() {
 			createButton.classList.add('button');
 			createButton.textContent = 'create account';
 
-			// Agregar evento de clic al botón de "crear cuenta"
-			createButton.addEventListener("click", this.handleCreatButton, );
-				console.log('Se hizo clic en el botón de "crear cuenta"');
 
-
-
+			
+			
+			
 			// Add the general div to the document body
 			document.body.appendChild(general);
 			this.shadowRoot.appendChild(general);
-
+			
 			const create = document.createElement('h4');
 			create.classList.add('h4');
 			create.textContent = 'Already have an ccount?';
-
+			
 			const up = document.createElement('h5');
 			up.textContent = 'Log in';
 			up.classList.add('h5');
 			up.addEventListener("click", this.handlelogButton, );
-				console.log('Se hizo clic en el elemento h5');
+			console.log('Se hizo clic en el elemento h5');
 			;
-
+			
 			const titulo = document.createElement('text');
 			titulo.textContent = 'Create an account';
 			titulo.classList.add('text');
-
+			
 			inputsDiv.appendChild(titulo);
 			inputsDiv.appendChild(create);
 			inputsDiv.appendChild(up);
@@ -134,11 +133,21 @@ handlelogButton() {
 			inputsDiv.appendChild(passwordInput);
 			inputsDiv.appendChild(createButton);
 			general.appendChild(inputsDiv);
+
+			// Agregar evento de clic al botón de "crear cuenta"
+			createButton.addEventListener("click", () => {
+			inputsDiv.appendChild(passwordInput);
+				const userId = registrarUsuario(userInput.value, parseInt(ageInput.value), parseInt(BenchpressInput.value), parseInt(DeadLiftInput.value), parseInt(SquatInput.value),  emailInput.value, passwordInput.value )
+				alert(`Usuario registrado con id: ${userId}`)
+			});
+			console.log('Se hizo clic en el botón de "crear cuenta"');
 		}
 
 		const cssProfile = this.ownerDocument.createElement('style');
 		cssProfile.innerHTML = styles;
 		this.shadowRoot?.appendChild(cssProfile);
+
+
 	}
 }
 
