4aa3430431e9ee82933865ba2804746ea906468d
dan0407 <duque.murillas@gmial.com>
2024-06-04 16:46:39 -0500

    finalisando melo

--- src/components/mycomponent/perfil/imps.ts
+++ src/components/mycomponent/perfil/imps.ts
@@ -3,7 +3,6 @@ import { appState, dispatch } from "../../../store/index";
 import { navigate } from "../../../store/actions";
 import { Screens } from "../../../types/trips";
 
-
 class perfil extends HTMLElement {
   constructor() {
     super();
@@ -18,7 +17,6 @@ class perfil extends HTMLElement {
     dispatch(navigate(Screens.DASHBOARD));
   }
 
-
   handleeditButton() {
     dispatch(navigate(Screens.EDIT));
   }
@@ -31,8 +29,7 @@ class perfil extends HTMLElement {
 
   render() {
     if (this.shadowRoot) {
-      this.shadowRoot.innerHTML = ` `;
-
+      this.shadowRoot.innerHTML = ``;
 
       // Create the inputs div
       const inputsDiv = document.createElement("div");
@@ -41,66 +38,61 @@ class perfil extends HTMLElement {
       const IMGDiv = document.createElement("div");
       IMGDiv.classList.add("dimg");
 
-      const contdiv = document.createElement("div");
-      contdiv.classList.add("dimg");
-
       // Create the login button
       const createButton = document.createElement("button");
       createButton.classList.add("button");
       createButton.textContent = "editprofile";
-	  createButton.addEventListener('click', () => {
-		createButton.addEventListener("click", this.handleeditButton);
-	});
+      createButton.addEventListener('click', () => {
+        createButton.addEventListener("click", this.handleeditButton);
+      });
 
-      
-      const create = document.createElement("title");
+      const create = document.createElement("h2");
       create.textContent = appState.userdata.username;
       create.classList.add("title");
 
       const ageLabelp = document.createElement('label');
-			ageLabelp.textContent = 'age';
+      ageLabelp.textContent = 'Age:';
       ageLabelp.classList.add("labeluno");
 
-      const userage = document.createElement("h9");
-	  userage.textContent = String(appState.userdata.age);
+      const userage = document.createElement("span");
+      userage.textContent = String(appState.userdata.age);
       userage.classList.add("h9");
 
       const benchpressLabelp = document.createElement('label');
-			benchpressLabelp.textContent = 'benchpress';
+      benchpressLabelp.textContent = 'Bench press:';
 
-      const userbenchpress = document.createElement("h9");
-	  userbenchpress.textContent = String(appState.userdata.benchpress);
-      userbenchpress.classList.add("h9")
+      const userbenchpress = document.createElement("span");
+      userbenchpress.textContent = `${appState.userdata.benchpress}kg`;
+      userbenchpress.classList.add("h1");
 
       const deadLiftLabelp = document.createElement('label');
-			deadLiftLabelp.textContent = 'deadLift';;
+      deadLiftLabelp.textContent = 'Deadlift:';
 
-      const userdeadLift = document.createElement("h9");
-	  userdeadLift.textContent = String(appState.userdata.deadLift);
-      userdeadLift.classList.add("h9");
+      const userdeadLift = document.createElement("span");
+      userdeadLift.textContent = `${appState.userdata.deadLift}kg`;
+      userdeadLift.classList.add("h2");
 
       const squatLabelp = document.createElement('label');
-			squatLabelp.textContent = 'squat';;
+      squatLabelp.textContent = 'Squat:';
 
-	  const usersquat = document.createElement("h9");
-      usersquat.textContent = String(appState.userdata.squat);
-      usersquat.classList.add("h9");
+      const usersquat = document.createElement("span");
+      usersquat.textContent = `${appState.userdata.squat}kg`;
+      usersquat.classList.add("h3");
 
-	  const perfil = document.createElement("img");
+      const perfil = document.createElement("img");
       perfil.src = appState.userdata.profile;
       perfil.classList.add("imgDOS");
 
       const logoutBtn = document.createElement('button');
-			logoutBtn.classList.add('button');
-			logoutBtn.textContent = 'logout';
-			logoutBtn.addEventListener('click', this.logout);
-		
-    
-      
+      logoutBtn.classList.add('button');
+      logoutBtn.textContent = 'logout';
+      logoutBtn.setAttribute('aria-label', 'logout');
+      logoutBtn.addEventListener('click', this.logout);
+
+      IMGDiv.appendChild(perfil);
       IMGDiv.appendChild(create);
       IMGDiv.appendChild(ageLabelp);
       IMGDiv.appendChild(userage);
-      IMGDiv.appendChild(perfil);
       inputsDiv.appendChild(benchpressLabelp);
       inputsDiv.appendChild(userbenchpress);
       inputsDiv.appendChild(deadLiftLabelp);
@@ -109,9 +101,8 @@ class perfil extends HTMLElement {
       inputsDiv.appendChild(usersquat);
       inputsDiv.appendChild(createButton);
       inputsDiv.appendChild(logoutBtn);
-      this.shadowRoot.appendChild(IMGDiv)
-      this.shadowRoot.appendChild(inputsDiv)
-
+      this.shadowRoot.appendChild(IMGDiv);
+      this.shadowRoot.appendChild(inputsDiv);
     }
 
     const cssProfile = this.ownerDocument.createElement("style");
